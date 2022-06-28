be108286cf0d4029d344119e0aa74024c8c093ce
AndreMorais98 <morais@pop-os.localdomain>
2022-06-28 21:56:47 +0100

    generate form

--- market/src/app.js
+++ market/src/app.js
@@ -4,15 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var bodyParser = require('body-parser')
+var indexRouter = require('./routes/index');
 require("dotenv-safe").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -23,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- market/src/routes/index.js
+++ market/src/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var json2csv = require('json2csv');
+const fs = require('fs');
+
+var parseUrl = require('body-parser')
+let encodeUrl = parseUrl.urlencoded({ extended: false })
+
 const serverUrl = process.env.REACT_APP_MORALIS_SERVER_URL;
 const appId = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 
@@ -20,6 +26,42 @@ router.get('/edit', function(req, res) {
 
 router.get('/create/step1', function(req, res) {
   res.render('step1', {serverUrl: serverUrl, appId: appId});
+});
+
+router.post('/step1' ,function(req, res) {
+  var name = req.body.name
+  var token = req.body.token
+  var number = req.body.number
+  var product = req.body.product
+  var clothes = req.body.clothes
+
+  var fields = [name, token, number, product, 'description', 'brand_id', 'product_id', 'made_in', 'color', 'composition'];
+  var fieldNames = ['Name', 'Abreviation Token', 'Number', 'Product', 'Descrição', 'Brand ID', 'Product ID', 'Made In', 'Color', 'Composition'];
+  
+  if (product == "Watch"){
+    fields.push('circumference', 'diameter', 'height', 'width');
+    fieldNames.push('Circumference', 'Diameter', 'Height', 'Width');
+  }
+  else if (product == "Jewellery"){
+    fields.push('circumference', 'length', 'width');
+    fieldNames.push('Circumference', 'Length', 'Width');
+  }
+  else if (product == "Clothes"){
+    fields.push(clothes, 'size');
+    fieldNames.push('Clothes', 'Size');
+  }
+  else if (product == "Shoes"){
+    fields.push('size');
+    fieldNames.push('Size');
+  }
+  else if (product == "Bags"){
+    fields.push('depth', 'handle', 'height', 'width');
+    fieldNames.push('Depth', 'Handle', 'Height', 'Width');
+  }
+  var data = json2csv({ data: docs, fields: fields, fieldNames: fieldNames });
+  res.attachment('filename.csv');
+  res.status(200).send(data);
+  res.redirect("/create/step2")
 });
 
 router.get('/create/step2', function(req, res) {
